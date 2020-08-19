10281e2ffabcc9eed3c86b02f8f2bd9a8532bef1
Tanishq0007 <tanishq16verma@gmail.com>
2020-08-19 21:05:33 +0530

    Add notes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,70 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" />
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      newItem: "",
+      list : []
+    }
+  }
+
+  addItem(todoValue){
+    if(todoValue !== "") {
+      const newItem = {
+        id: Date.now(),
+        isDone: false
+      };
+      const list = [...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        newItem: ""
+      });
+    }
+  }
+
+  deleteItem(id){
+    const list = [...this.state.list];
+    const updatedlist = list.filter(item => item.id !== id);
+    this.setState({list: updatedlist})
+  }
+
+  Updateinput(input){
+    this.setState({newItem:input});
+  }
+
+  render(){
+    return (
+      <div>
+        <img src={logo} width="100" height="100" className="logo" />
+        <h1 className="app-title">ToDo App</h1>
+        <div className="container">
+          Add an item....
+          <br />
+          <input 
+          type="text" 
+          className="input-text"
+          placeholder="Write a Todo"/>
+          <button className="add-btn">Add Todo</button>
+          <div className="list">
+            <ul>
+              <li>
+                <input type="checkbox"/>
+                Record youtube videos
+                <button className="btn">Delete</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
